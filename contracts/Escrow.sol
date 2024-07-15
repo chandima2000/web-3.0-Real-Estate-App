a00d3f17f6b4abecd25d27821b1eba4a3a394059
chandima2000 <hychandima2000@gmail.com>
2024-07-15 09:28:37 +0530

    create property Listing functionality

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+
+//  transferring NFTs between addresses
 interface IERC721 {
     function transferFrom(
         address _from,
@@ -9,11 +11,23 @@ interface IERC721 {
     ) external;
 }
 
+
+// Handles the escrow process for real estate transactions using NFTs
 contract Escrow {
     address public lender;
     address public inspector;
     address payable public seller;  // Sellor recievs crypto currency
     address public nftAddress;
+
+    modifier onlySeller() {
+        require(msg.sender == seller,"Only seller can call this method");
+        _;
+    }
+
+    mapping(uint256 => bool) public isListed;
+    mapping(uint256 => uint256) public purchasePrice;
+    mapping(uint256 => uint256) public escrowAmount;
+    mapping(uint256 => address) public buyer;
 
     constructor (
         address _nftAddress,
@@ -26,4 +40,24 @@ contract Escrow {
         seller = _seller;
         nftAddress = _nftAddress;
     }
+
+    
+    //  listing of a property by transferring the NFT from the caller's address to the escrow contract.
+    function propertyList (
+            uint256 _nftID, // _nftID: The ID of the NFT representing the property.  
+            uint256 _purchasePrice, 
+            uint256 _escrowAmount, 
+            address _buyer
+        ) public payable onlySeller{
+        // Transfers the specified NFT from the seller to the escrow contract.
+        IERC721(nftAddress).transferFrom(msg.sender, address(this), _nftID);
+
+        isListed[_nftID] = true;
+        purchasePrice[_nftID] = _purchasePrice;
+        escrowAmount[_nftID] = _escrowAmount;
+        buyer[_nftID] = _buyer;
+
+    }
+
+
 }
