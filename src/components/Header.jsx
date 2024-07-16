9612dbf7ca9516c8a721788c736971cebeb324f4
chandima2000 <hychandima2000@gmail.com>
2024-07-16 16:40:57 +0530

    update navigation bar

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Search = () => {
+    return (
+        <header>
+            <h2 className="header__title-1">Discover, Invest, and Own Your Dream Property</h2>
+            <h2 className="header__title-2">Search it. Explore it. Buy it.</h2> 
+        </header>
+    );
+}
+
+export default Search;
