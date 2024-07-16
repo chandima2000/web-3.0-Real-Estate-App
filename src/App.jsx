002a999ccad10fc356340547ae96fc9aebf0b7fe
chandima2000 <hychandima2000@gmail.com>
2024-07-16 16:42:24 +0530

    create Home page

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { ethers } from 'ethers';
 
 // Components
 import Navigation from './components/Navigation';
-import Search from './components/Search';
+import Header from './components/Header';
 import Home from './components/Home';
 
 // ABIs
@@ -14,28 +14,99 @@ import Escrow from './abis/Escrow.json'
 import config from './config.json';
 
 function App() {
+  const [provider, setProvider] = useState(null)
+  const [escrow, setEscrow] = useState(null)
 
-  const [account, setAccount] = useState([]);
+  const [account, setAccount] = useState(null)
+
+  const [homes, setHomes] = useState([])
+  const [home, setHome] = useState({})
+  const [toggle, setToggle] = useState(false);
 
   const loadBlockchainData = async () => {
+
+    // Initializing the Ethereum provider.
     const provider = new ethers.BrowserProvider(window.ethereum)
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-    setAccount(accounts[0]);
-    console.log(account[0])
+    setProvider(provider)
+    const network = await provider.getNetwork()
+
+    // Connecting to the RealEstate smart contract.
+    const realEstate = new ethers.Contract(config[network.chainId].realEstate.address, RealEstate, provider)
+    const totalSupply = await realEstate.totalSupply() // Fetch the total supply 
+
+    // An array of properties
+    const homes = []
+
+    for (var i = 1; i <= totalSupply; i++) {
+      const uri = await realEstate.tokenURI(i)
+      const response = await fetch(uri)
+      const metadata = await response.json()
+      homes.push(metadata)
+    }
+
+    setHomes(homes)
+
+    // Connecting to the Escrow smart contract
+    const escrow = new ethers.Contract(config[network.chainId].escrow.address, Escrow, provider)
+    setEscrow(escrow)
+
+    // Interact with the Ethereum blockchain.
+    window.ethereum.on('accountsChanged', async () => {
+
+      // Get currently available accounts from the Ethereum provider.
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' }); 
+
+      // Normalizes to standard Ethereum address
+      const account = ethers.getAddress(accounts[0])
+      setAccount(account);
+    })
   }
-  
+
+
   useEffect(() => {
-    loadBlockchainData();
-  },[]);
+    loadBlockchainData()
+  }, [])
+
+  const togglePop = (home) => {
+    setHome(home)
+    toggle ? setToggle(false) : setToggle(true);
+  }
 
   return (
     <div>
+      <Navigation account={account} setAccount={setAccount} />
+      <Header/>
 
       <div className='cards__section'>
-        <Navigation/>
-        <h3>Welcome to Real Estate</h3>
+
+        <h3>Homes For You</h3>
+
+        <hr />
+
+        <div className='cards'>
+          {homes.map((home, index) => (
+            <div className='card' key={index} onClick={() => togglePop(home)}>
+              <div className='card__image'>
+                <img src={home.image} alt="Home" />
+              </div>
+              <div className='card__info'>
+                <h4>{home.attributes[0].value} ETH</h4>
+                <p>
+                  <strong>{home.attributes[2].value}</strong> beds |
+                  <strong>{home.attributes[3].value}</strong> ba.rooms |
+                  <strong>{home.attributes[4].value}</strong> sqft
+                </p>
+                <p>{home.address}</p>
+              </div>
+            </div>
+          ))}
+        </div>
 
       </div>
+
+      {toggle && (
+        <Home home={home} provider={provider} account={account} escrow={escrow} togglePop={togglePop} />
+      )}
 
     </div>
   );
